ef02735af946926e8f4fe3d122cebc7e767314e9
muhammadmazlan <binmazlanmuhammad@gmail.com>
2020-12-05 23:56:54 +0800

    add appBar

---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppLogo from '../AppLogo/AppLogo';
+import NavItem from '../NavItem/NavItem'
+
+export default function AppBar() {
+    return (
+        <div>
+            <AppLogo />
+            <NavItem />
+        </div>
+    )
+}
