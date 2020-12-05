41834aaf190af1324ec0d9f2fad9f5b93b895286
muhammadmazlan <binmazlanmuhammad@gmail.com>
2020-12-05 23:57:49 +0800

    add Netflix Component

---
+++ src/components/Netflix/Netflix.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import AppBar from '../AppBar/AppBar'
+
+
+export default class Netflix extends Component {
+    render() {
+        return (
+            <div>
+                <AppBar />
+            </div>
+        )
+    }
+}
